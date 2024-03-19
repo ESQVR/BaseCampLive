a3d4a455c60cedb0eba7c89068b3d63876b08a1d
ChristopherDeetz <chrisqwasar@gmail.com>
2024-03-19 12:17:38 -0700

    Session 3 complete, no method error from the end of the session resolved: missing @ in view template loop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "projects", to: 'projects#index'
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,4 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  resources :projects
 end
