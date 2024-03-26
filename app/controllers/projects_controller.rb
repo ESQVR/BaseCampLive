711c80e1cd1c487b641e9d719390fd9ce8dcb4b7
ChristopherDeetz <chrisqwasar@gmail.com>
2024-03-26 16:14:15 -0700

    Session 3 complete - last stop session 4

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,11 +4,27 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(permitted_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+  end
+
+  private
+  def permitted_params
+    params.require(:project).permit(:title, :description)
   end
 end
